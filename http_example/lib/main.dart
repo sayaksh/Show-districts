e973fc3348eccc2fb5bd9684188dadec0982fecb
sayaksh <kshsayadevi@gmail.com>
2022-07-08 10:17:19 +0530

    show districts with alertdialog

--- http_example/lib/main.dart
+++ http_example/lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:http_example/indexForm.dart';
 
 import 'package:http_example/model/districts.dart';
 
